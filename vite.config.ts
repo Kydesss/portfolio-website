68d494efc1dea6aa9723aab1fe74524d9dac186f
Joaquin Pacia <joaquin.pacia@mail.utoronto.ca>
2025-07-18 20:35:56 -0400

    fix: update Vite base path configuration to match GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/",
+    base: "/kydesss.github.io/portfolio-website/",
     plugins: [react(), tailwindcss()],
 });
